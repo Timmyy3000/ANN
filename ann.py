7dba997956a3d776810afd51a01c047c2f03719a
Timmyy3000 <timiogunme@gmail.com>
2021-06-02 19:15:14 +0100

    Rread dataset

--- ann.py
+++ ann.py
@@ -1,5 +1,8 @@
 # importing libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import  tensorflow as tf
+
+dataset = pd.read_csv("Churn_Modelling.csv")
+
